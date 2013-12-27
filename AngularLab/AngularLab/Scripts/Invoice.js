df2e834aa6193e678968d6902a1124af1c5b1473
devesh <deveshkrsingh@hotmail.com>
2013-12-27 12:55:33 +0530

    jkl

---
+++ AngularLab/AngularLab/Scripts/Invoice.js
@@ -0,0 +1,12 @@
+﻿angular.module("invoice1", [])
+       .controller("InvoiceController", function ()
+       {
+           this.Qty = 1;
+           this.Cost = 2;
+           this.inCurr = 'USD';
+           this.Currencies = ['USD', 'CNY', 'EUR'];
+           this.Pay = function () {
+               alert('Thanks');
+           };
+       }
+       )
